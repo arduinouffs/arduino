d5c24076660f85a5e1f56d0c2cd674c4e13d8911
alexszds <alexszds@gmail.com>
2022-02-01 11:58:42 -0300

    sahhah

---
+++ Projeto prático/internet_js.ino/internet_js.ino.ino
@@ -0,0 +1,330 @@
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    #define qtdePortasDigitaisDoArduino 53
+#else
+    #define qtdePortasDigitaisDoArduino 13
+#endif
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };   //Definição do MAC Address da Ethernet Shield
+IPAddress ip(169, 254, 22, 100);                       // http://169.254.22.100 - Definição do endereço IP que a Ethernet Shield terá na rede onde estiver conectada. Verifique a Máscara-de-subrede e todos os equipamentos conectados na rede para achar um IP compatível e livre.
+EthernetServer server(80);                             // Porta em que a Ethernet Shield irá receber as requisições das páginas (o padrão WEB é a porta 80)
+
+String HTTP_req; 
+String URLValue;
+
+void processaPorta(byte porta, byte posicao, EthernetClient cl);
+void lePortaDigital(byte porta, byte posicao, EthernetClient cl);          //<----- NOVO
+void lePortaAnalogica(byte porta, byte posicao, EthernetClient cl);        //<----- NOVO
+String getURLRequest(String *requisicao);
+bool mainPageRequest(String *requisicao);
+
+const byte qtdePinosDigitais = 11;
+byte pinosDigitais[qtdePinosDigitais] = {2           , 3     , 5     , 6     , 7     , 8     , 9     , A2    , A3    , A4    , A5};
+byte modoPinos[qtdePinosDigitais]     = {INPUT_PULLUP, OUTPUT, OUTPUT, OUTPUT, OUTPUT, OUTPUT, OUTPUT, OUTPUT, OUTPUT, OUTPUT, OUTPUT};
+
+const byte qtdePinosAnalogicos = 2;
+byte pinosAnalogicos[qtdePinosAnalogicos] = {A0, A1};
+
+void setup()
+{ 
+
+    Ethernet.begin(mac, ip); 
+    server.begin();           
+
+    Serial.begin(9600);
+
+    for (int nP=0; nP < qtdePinosDigitais; nP++) {
+        pinMode(pinosDigitais[nP], modoPinos[nP]);
+    }
+}
+
+void loop()
+{
+
+    EthernetClient client = server.available();
+
+    if (client) { 
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) { 
+                char c = client.read(); 
+                HTTP_req += c;  
+                
+                if (c == '\n' && currentLineIsBlank ) { 
+
+                    if ( mainPageRequest(&HTTP_req) ) {
+                        URLValue = getURLRequest(&HTTP_req);
+                        Serial.println(HTTP_req);
+                                                 
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-Type: text/html");
+                        client.println("Connection: keep-alive");              //<------ ALTERADO 
+                        client.println();
+                        
+                        //Conteudo da Página HTML
+                        client.println("<!DOCTYPE html>");
+                        client.println("<html>");
+
+                        
+                        client.println("<head>");
+                        client.println("<title>Arduino via WEB</title>");
+
+                        client.println("<script>");
+                        client.println("function LeDadosDoArduino() {");
+                        client.println("nocache = \"&nocache=\" + Math.random() * 1000000;");
+                        client.println("var request = new XMLHttpRequest();");
+                        client.println("var posIni;");
+                        client.println("var valPosIni;");
+                        client.println("var valPosFim;");
+                        client.println("request.onreadystatechange = function() {");
+                        client.println("if (this.readyState == 4) {");
+                        client.println("if (this.status == 200) {");
+                        client.println("if (this.responseText != null) {");
+
+                        for (int nL=0; nL < qtdePinosDigitais; nL++) {                                                    //<-------NOVO
+                            client.print("posIni = this.responseText.indexOf(\"PD");
+                            client.print(pinosDigitais[nL]);
+                            client.println("\");");
+                            client.println("if ( posIni > -1) {");
+                            client.println("valPosIni = this.responseText.indexOf(\"#\", posIni) + 1;");
+                            client.println("valPosFim = this.responseText.indexOf(\"|\", posIni);");
+                            client.print("document.getElementById(\"pino");
+                            client.print(pinosDigitais[nL]);
+                            client.println("\").checked = Number(this.responseText.substring(valPosIni, valPosFim));");
+                            client.println("}");
+                        }
+
+                        for (int nL=0; nL < qtdePinosAnalogicos; nL++) {                                                    //<-------NOVO
+                            
+                            client.print("posIni = this.responseText.indexOf(\"PA");
+                            client.print(pinosAnalogicos[nL]);
+                            client.println("\");"); 
+                            client.println("if ( posIni > -1) {");
+                            client.println("valPosIni = this.responseText.indexOf(\"#\", posIni) + 1;");
+                            client.println("valPosFim = this.responseText.indexOf(\"|\", posIni);");
+                            client.print("document.getElementById(\"pino");
+                            client.print(pinosAnalogicos[nL]);
+                            client.print("\").innerHTML = \"Porta A");
+                            client.print(pinosAnalogicos[nL] - (qtdePortasDigitaisDoArduino + 1));
+                            client.print(" - Valor: \" + this.responseText.substring(valPosIni, valPosFim);");
+                            client.println("}");
+                        }
+                          
+                        client.println("}}}}");
+                        client.println("request.open(\"GET\", \"solicitacao_via_ajax\" + nocache, true);");
+                        client.println("request.send(null);");
+                        client.println("setTimeout('LeDadosDoArduino()', 1000);");
+                        client.println("}");
+                        client.println("</script>");
+                        
+                        client.println("</head>");
+
+                        client.println("<body onload=\"LeDadosDoArduino()\">");                      //<------ALTERADO                    
+                        client.println("<h1>PORTAS EM FUN&Ccedil;&Atilde;O ANAL&Oacute;GICA</h1>");
+
+                        for (int nL=0; nL < qtdePinosAnalogicos; nL++) {
+
+                            client.print("<div id=\"pino");                         //<----- NOVO
+                            client.print(pinosAnalogicos[nL]);
+                            client.print("\">"); 
+                                                         
+                            client.print("Porta A");
+                            client.print(pinosAnalogicos[nL] - (qtdePortasDigitaisDoArduino + 1));
+                            client.println(" - Valor: ");
+                               
+                            client.print( analogRead(pinosAnalogicos[nL]) );
+                            client.println("</div>");                               //<----- NOVO
+                               
+                            client.println("<br/>");                             
+                        }
+                        
+                        client.println("<br/>");                        
+                        client.println("<h1>PORTAS EM FUN&Ccedil;&Atilde;O DIGITAL</h1>");
+                        client.println("<form method=\"get\">");
+
+                        for (int nL=0; nL < qtdePinosDigitais; nL++) {
+                            processaPorta(pinosDigitais[nL], nL, client);
+                            client.println("<br/>");
+                        }
+                        
+                        client.println("<br/>");
+                        client.println("<button type=\"submit\">Envia para o Arduino</button>");
+                        client.println("</form>");                      
+                        
+                        client.println("</body>");
+                        
+                        client.println("</html>");
+
+                    
+                    } else if (HTTP_req.indexOf("solicitacao_via_ajax") > -1) {     //<----- NOVO
+
+                        Serial.println(HTTP_req);
+
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-Type: text/html");
+                        client.println("Connection: keep-alive");      
+                        client.println();                      
+
+                        for (int nL=0; nL < qtdePinosAnalogicos; nL++) {
+                            lePortaAnalogica(pinosAnalogicos[nL], nL, client);                            
+                        }
+                        for (int nL=0; nL < qtdePinosDigitais; nL++) {
+                            lePortaDigital(pinosDigitais[nL], nL, client);
+                        }
+                            
+                    } else {
+
+                        Serial.println(HTTP_req);
+                        client.println("HTTP/1.1 200 OK");
+                    }
+                    HTTP_req = "";    
+                    break;
+                }
+                
+                if (c == '\n') {
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    currentLineIsBlank = false;
+                }
+            }
+        } 
+        delay(1);     
+        client.stop(); 
+    } 
+}
+
+
+void processaPorta(byte porta, byte posicao, EthernetClient cl)
+{
+static boolean LED_status = 0;
+String cHTML;
+
+    cHTML = "P";
+    cHTML += porta;
+    cHTML += "=";
+    cHTML += porta;
+
+    if (modoPinos[posicao] == OUTPUT) { 
+        
+        if (URLValue.indexOf(cHTML) > -1) { 
+           LED_status = HIGH;
+        } else {
+           LED_status = LOW;
+        }
+        digitalWrite(porta, LED_status);
+    } else {
+
+        LED_status = digitalRead(porta);
+    }
+        
+    cl.print("<input type=\"checkbox\" name=\"P");
+    cl.print(porta);
+    cl.print("\" value=\"");
+    cl.print(porta);
+    
+    cl.print("\"");
+
+    cl.print(" id=\"pino");           //<------NOVO
+    cl.print(porta);
+    cl.print("\"");
+    
+    if (LED_status) { 
+        cl.print(" checked ");
+    }
+
+    if (modoPinos[posicao] != OUTPUT) { 
+        cl.print(" disabled ");
+    }
+    
+    cl.print(">Porta ");
+
+    if (porta <= 13) {
+       cl.print(porta);
+    } else {
+       cl.print("A");
+       cl.print(porta - (qtdePortasDigitaisDoArduino + 1));
+    }
+    cl.println();
+}
+
+void lePortaDigital(byte porta, byte posicao, EthernetClient cl)
+{
+    if (modoPinos[posicao] != OUTPUT) { 
+       cl.print("PD");
+       cl.print(porta);
+       cl.print("#");
+       
+       if (digitalRead(porta)) {
+          cl.print("1");
+       } else {
+          cl.print("0");
+       }
+       cl.println("|");
+    }
+}
+
+
+void lePortaAnalogica(byte porta, byte posicao, EthernetClient cl)
+{
+   cl.print("PA");
+   cl.print(porta);
+   cl.print("#");
+   
+   cl.print(analogRead(porta));
+
+   //especifico para formatar o valor da porta analogica A0
+   if (porta == A0) { 
+      cl.print(" (");
+      cl.print(map(analogRead(A0),0,1023,0,179)); 
+      cl.print("&deg;)");
+   }
+
+   //especifico para formatar o valor da porta analogica A1
+   if (porta == A1) { 
+      cl.print(" (");
+      float voltagem = analogRead(A1) * 5.0 / 1024.0;
+      cl.print( (voltagem - 0.5) * 10 );      
+      cl.print("&deg;C)");
+   }
+   
+   cl.println("|");   
+}
+
+
+String getURLRequest(String *requisicao) {
+int inicio, fim;
+String retorno;
+
+  inicio = requisicao->indexOf("GET") + 3;
+  fim = requisicao->indexOf("HTTP/") - 1;
+  retorno = requisicao->substring(inicio, fim);
+  retorno.trim();
+
+  return retorno;
+}
+
+bool mainPageRequest(String *requisicao) {
+String valor;
+bool retorno = false;
+
+  valor = getURLRequest(requisicao);
+  valor.toLowerCase();
+
+  if (valor == "/") {
+     retorno = true;
+  }
+
+  if (valor.substring(0,2) == "/?") {
+     retorno = true;
+  }  
+
+  if (valor.substring(0,10) == "/index.htm") {
+     retorno = true;
+  }  
+
+  return retorno;
+}
