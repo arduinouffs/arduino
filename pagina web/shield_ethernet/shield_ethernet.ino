51dfa42b423f4c9371d21dcfcf34e0417061f549
alexszds <alexszds@gmail.com>
2022-02-03 10:25:21 -0300

    ards

--- pagina web/shield_ethernet/shield_ethernet.ino
+++ pagina web/shield_ethernet/shield_ethernet.ino
@@ -1,17 +1,47 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SD.h>
+#include <DHT.h>
+
+#define DHTPIN A1 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT  
+
+String HTTP_req; 
+String URLValue;
+
+String getURLRequest(String *requisicao);
+bool mainPageRequest(String *requisicao);
+void tempUmid(EthernetClient cl);
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 100, 40); // Endereço IP que a Ethernet Shield terá. Deve ser alterado para um endereço livre da sua rede.
 EthernetServer server(80);     // Cria um servidor WEB
+File webFile;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
     Ethernet.begin(mac, ip);  // Inicializa a Ethernet Shield
     server.begin();           // Inicia esperando por requisições dos clientes (Browsers)
+    Serial.begin(9600);
+    dht.begin();
+
+    Serial.println("Iniciando o cartao SD...");
+    if (!SD.begin(4)) {
+        Serial.println("ERRO - a inicializacao do cartao SD falhou!");
+        return;    //aborta a função SETUP caso exista erro
+    }
+    
+    Serial.println("Cartao iniciado com SUCESSO!!");
+    // verifica a existencia do arquivo 1~1.htm no cartao SD
+    if (!SD.exists("1~1.htm")) {
+        Serial.println("ERRO - O arquivo 1~1.htm nao foi localizado!");
+        return;   //aborta a função SETUP caso exista erro
+    }
+    
+    Serial.println("Arquivo 1~1.htm encontrado!");
 } // fim do setup
 
 void loop() {
-
     EthernetClient client = server.available();  // Tenta pegar uma conexão com o cliente (Browser)
 
     if (client) {  // Existe um cliente em conexão ?
@@ -28,27 +58,20 @@ void loop() {
                     // envia o cabeçalho de uma resposta http padrão
                     client.println("HTTP/1.1 200 OK");
                     client.println("Content-Type: text/html");
-                    client.println("Connection: close");
+                    client.println("Connection: keep-alive");
                     client.println();
                     
                     // ENVIA A PÁGINA WEB
-                    client.println("<!DOCTYPE html>");
-                        client.println("<html>");                        
-                        client.println("<head>");
-                          client.println("<title>Página Arduino</title>");
-                          client.println("<meta charset='UTF-8'>");
-                          client.println("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
-                          client.println("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-                          client.println("<style>.temp_umid {font-size: 50px;}</style>");
-                          client.println("</head><body>");
-                            client.println("<h1>Controle de temperatura e umidade</h1>");
-                            client.println("<div><div>");
-                              client.println("<h3>Temperatura</h3><div class='temp_umid'><span id='temperatura'>10</span>°C</div>");
-                              client.println("</div><div>");
-                              client.println("<h3>Umidade</h3><div class='temp_umid'><span id='umidade'>80</span>%</div>");
-                          client.println("</div></div></body></html>");  
+                    webFile = SD.open("1~1.htm");        // abre o arquivo da pagina WEB
+                    if (webFile) {
+                        while(webFile.available()) {
+                            client.write(webFile.read());  // envia a pagina WEB para o cliente (browser)
+                        }
+                        webFile.close();
+                    }
                     break;
                 }
+                
                 // toda linha de texto recebida do cliente termina com os caracteres \r\n
                 if (c == '\n') {
                     // ultimo caractere da linha do texto recebido
