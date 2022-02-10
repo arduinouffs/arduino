da0de007901912b4b7a6e2706373bb31af37b76b
alexszds <alexszds@gmail.com>
2022-02-10 11:58:41 -0300

    a

--- Projeto prático/infravermelho/infravermelho.ino
+++ Projeto prático/infravermelho/infravermelho.ino
@@ -1,20 +1,20 @@
 // INCLUSÃO DE BIBLIOTECA
 #include <IRremote.h>
-#include <PushButton.h>
+//#include <PushButton.h>
 
 // DIRETIVAS DE COMPILAÇÃO
 #define tempoTecla 350
 #define frequencia 38 // kHz
 
 // DEFINIÇÃO DOS PINOS
-#define pinReceptor 11
-#define pinBot1 8
+#define pinReceptor 5
+//#define pinBot1 8
 #define pinLed 12
 
 // INSTANCIANDO OBJETOS
 IRrecv receptorIR(pinReceptor);
 IRsend emissorIR;
-PushButton botaoLeitura(pinBot1);
+//PushButton botaoLeitura(pinBot1);
 
 // DECLARAÇÃO VARIÁVEIS GLOBAIS
 bool lerComando = false;
@@ -29,8 +29,8 @@ void dumpCode (decode_results *results);
 
 //  DECLARAÇÃO DAS TECLAS CLONADAS
 
-unsigned int teclaA[] = {3900,3950, 450,2000, 450,2000, 400,2000, 500,2000, 400,1050, 450,1050, 450,2000, 450,2000, 450,1050, 400,1050, 450,1050, 450,2000, 450,1050, 450,1050, 400,1050, 450,1050, 450,2000, 400,2000, 500,1000, 450,1050, 450,2000, 450,2000, 450,2000, 400,1050, 400};
-unsigned int teclaB[] = {8900, 4500, 500, 650, 450, 650, 500, 600, 500, 600, 500, 650, 450, 650, 450, 650, 450, 650, 450, 1750, 500, 600, 500, 1700, 500, 1700, 500, 1750, 500, 1700, 500, 650, 450, 1750, 450, 650, 500, 600, 500, 1700, 500, 1700, 500, 650, 450, 650, 500, 600, 500, 600, 500, 1700, 500, 1750, 500, 600, 500, 600, 500, 1700, 500, 1700, 500, 1750, 500, 1700, 500};
+unsigned int desliga[100] = {500,1650, 600,1600, 500,1650, 550,1750, 400,1650, 550,1650, 500,1700, 500,1650, 500,700, 450,650, 450,700, 450,650, 450,650, 450,700, 450,650, 400,700, 450,1650, 550,1800, 350,1650, 550,1750, 400,1650, 550,1650, 450,1750, 500,1650, 500,700, 450,650, 450,650, 450,700, 450,650, 400,700, 450,700, 450,650, 450,1650, 550,1650, 500,1650, 550,1800, 350,1650, 550,1650, 500,1650, 550,1650, 500,700, 450,650, 450,650, 450,700, 450,650, 450,650, 450,650, 450,650, 550,1600, 600,1600};
+unsigned int liga[83] = {250,1900, 250,1950, 250,1900, 200,2000, 250,1900, 250,1950, 250,1900, 300,850, 250,900, 200,900, 200,900, 250,900, 200,900, 250,850, 250,850, 250,1900, 300,1950, 200,1850, 350,1850, 300,1800, 400,1800, 350,1800, 400,1750, 350,600, 600,500, 600,550, 550,550, 600,650, 350,800, 450,700, 300,850, 350,1750, 350,1900, 350,1800, 350,1900, 250,1900, 300,2000, 150,1950, 200,2000, 200,950, 100,1050, 100};
 unsigned int teclaC[] = {8900,4500, 500,600, 500,650, 450,650, 450,650, 450,650, 450,650, 500,600, 500,600, 500,1700, 500,600, 500,1750, 500,1700, 500,1700, 500,1700, 500,650, 450,1750, 500,600, 500,600, 500,600, 500,1700, 500,650, 450,650, 500,600, 500,600, 500,1700, 500,1750, 450,1750, 500,600, 500,1750, 450,1750, 450,1750, 500,1700, 500};
 unsigned int teclaD[] = {/*COPIE_OS_DADOS_DENTRO_DAS_CHAVES_DO_MONITOR_SERIAL_E_COLE_AQUI*/};
 
@@ -45,17 +45,17 @@ void setup() {
 }
 
 void loop() {
-  // MÉTODO PARA LEITURA E ATUALIZAÇÃO DAS PROPRIEDADES DO BOTÃO
-  botaoLeitura.button_loop();
-
-  // BLOCO CONDIÇÕES PARA INICIAR LEITURA
-  if (botaoLeitura.pressed() && !lerComando) {
-    lerComando = true;
-    digitalWrite(pinLed, HIGH);
-  } else if (botaoLeitura.pressed() && lerComando) {
-    lerComando = false;
-    digitalWrite(pinLed, LOW);
-  }
+//  // MÉTODO PARA LEITURA E ATUALIZAÇÃO DAS PROPRIEDADES DO BOTÃO
+//  botaoLeitura.button_loop();
+//
+//  // BLOCO CONDIÇÕES PARA INICIAR LEITURA
+//  if (botaoLeitura.pressed() && !lerComando) {
+//    lerComando = true;
+//    digitalWrite(pinLed, HIGH);
+//  } else if (botaoLeitura.pressed() && lerComando) {
+//    lerComando = false;
+//    digitalWrite(pinLed, LOW);
+//  }
 
   // LAÇO PARA LEITURA DO RECEPTOR IR QUANDO FOR PRESSIONADO O BOTÃO
   while (lerComando) {
