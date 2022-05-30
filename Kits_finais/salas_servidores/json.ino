5a4fac9d578476fa9649e66dfefb3e629ea19737
arduinouffs <arduinouffsch@gmail.com>
2022-05-30 11:51:05 -0300

    Up

--- Kits_finais/salas_servidores/json.ino
+++ Kits_finais/salas_servidores/json.ino
@@ -4,42 +4,20 @@
 #define NOBREAK_SAIDA_PIN A2
 #define VOLT_CAL_ENTRADA 458
 #define VOLT_CAL_SAIDA 503
-#define pin2 A4 // Vout2 PM10
-#define pin1 2 // Vout1 PM2
 
 extern float read_temperature();
 extern float read_humidity();
 extern float read_voltage(int PIN, float VOLT_CAL);
 extern void inicialize_dht();
+extern String read_ppm();
 
 extern bool ar_condicionado;
 extern bool dehumidify;
 extern EthernetClient client;
 
-#define sampletime_ms 3000 //sampe 1s ;
-unsigned long duration1;
-unsigned long duration2;
-unsigned long lowpulseoccupancy1 = 0;
-unsigned long lowpulseoccupancy2 = 0;
-float ratio1 = 0;
-float ratio2 = 0;
-float pm2p5 = 0;
-float pm10 = 0;
-
 void json_generator(EthernetClient client) {
   client.println("HTTP/1.1 200 OK\nContent-Type:aplication/json\nContent-Disposition:attachment;filename=\"data.json\"\nConnection:close\n");
-  
-  duration1 = pulseIn(pin1, LOW);
-  duration2 = pulseIn(pin2, LOW);
-  lowpulseoccupancy1 = lowpulseoccupancy1+duration1;
-  lowpulseoccupancy2 = lowpulseoccupancy2+duration2;
-  
-  ratio1 = lowpulseoccupancy1/(sampletime_ms*10.0);  // Integer percentage 0=>100
-  pm2p5 = 1.1*pow(ratio1,3)-3.8*pow(ratio1,2)+520*ratio1+0.62; // using spec sheet curve
-  
-  ratio2 = lowpulseoccupancy2/(sampletime_ms*10.0);  // Integer percentage 0=>100
-  pm10 = 1.1*pow(ratio2,3)-3.8*pow(ratio2,2)+520*ratio2+0.62;
-  
+    
   String request = "";
   request += "{\"temp\":\"";
   request += read_temperature();
@@ -50,11 +28,8 @@ void json_generator(EthernetClient client) {
   request += "\",\"fumaca\":\"";
   if (!digitalRead(MQ2PIN_DIGITAL)) request += "1";
   else request += "0";
-  request += "\",\"ar\":{\"pm2p5\":\"";
-  request += pm2p5;
-  request += "\",\"pm10\":\"";
-  request += pm10;
-  request += "\"},\"noin\":\"";
+  request += read_ppm();
+  request += ",\"noin\":\"";
   request += read_voltage(NOBREAK_ENTRADA_PIN, VOLT_CAL_ENTRADA);
   request += "\",\"noout\":\"";
   request += read_voltage(NOBREAK_SAIDA_PIN, VOLT_CAL_SAIDA);;
